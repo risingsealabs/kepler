c6bad294c042e54a610b4c21cf3d3a9c1f94b5c8
Alexandre Esteves <alexfmpe@proton.me>
2024-04-26 16:53:58 +0100

    Cleanup

--- nix/project.nix
+++ nix/project.nix
@@ -1,11 +1,11 @@
 { }:
 let
-  pkgs = import (builtins.fetchTarball {
+  nixpkgs = import (builtins.fetchTarball {
     url = https://github.com/nixos/nixpkgs/archive/4c86138ce486d601d956a165e2f7a0fc029a03c1.tar.gz;
     sha256 = "sha256:0bw84ndw6f82yapkkpqnqbv1izyys3y79nlmg04576z53ccggjgb";
   }) { inherit overlays; };
 
-  gitignore = pkgs.fetchFromGitHub {
+  gitignore = nixpkgs.fetchFromGitHub {
     owner = "hercules-ci";
     repo = "gitignore";
     rev = "f9e996052b5af4032fe6150bba4a6fe4f7b9d698";
@@ -13,9 +13,9 @@ let
   };
   gitignoreSource = (import gitignore {}).gitignoreSource;
 
-  iavl = pkgs.callPackage ./iavl.nix {};
+  iavl = nixpkgs.callPackage ./iavl.nix {};
 
-  tendermint = (pkgs.callPackage ./tendermint.nix {}).overrideAttrs { __darwinAllowLocalNetworking = true; };
+  tendermint = (nixpkgs.callPackage ./tendermint.nix {}).overrideAttrs { __darwinAllowLocalNetworking = true; };
 
   root = ../.;
 
@@ -32,7 +32,7 @@ let
   };
 
   repos = {
-    avl-auth = pkgs.fetchFromGitHub {
+    avl-auth = nixpkgs.fetchFromGitHub {
       owner  = "oscoin";
       repo   = "avl-auth";
       rev    = "dfc468845a82cdd7d759943b20853999bc026505";
@@ -40,14 +40,14 @@ let
     };
 
     # https://github.com/haskell-grpc-native/http2-client/pull/92#issuecomment-2079254415
-    http2-client = pkgs.fetchFromGitHub {
+    http2-client = nixpkgs.fetchFromGitHub {
       owner  = "haskell-grpc-native";
       repo   = "http2-client";
       rev    = "0140d8fc21e240325a19da4ce641c346df5f5079";
       sha256 = "sha256-umw1EJ9NCfu5LrAbD/s4IKFFtUhIkIt4MwrOfedMf/0=";
     };
 
-    http2-grpc-haskell = pkgs.fetchFromGitHub {
+    http2-grpc-haskell = nixpkgs.fetchFromGitHub {
       owner  = "haskell-grpc-native";
       repo   = "http2-grpc-haskell";
       rev    = "7c19009e37fc305f73988b06b2cba9f31ae5478e";
@@ -55,7 +55,7 @@ let
     };
 
     # https://github.com/well-typed/large-records/pull/151
-    large-records = pkgs.fetchFromGitHub {
+    large-records = nixpkgs.fetchFromGitHub {
       owner  = "TristanCacqueray";
       repo   = "large-records";
       rev    = "7576d3de071d775901073ab4c714b984fab84c95";
@@ -63,11 +63,7 @@ let
     };
   };
 
-  repoPackages = {
-    inherit (repos) avl-auth;
-  };
-
-  extra-build-inputs = with pkgs; {
+  extra-build-inputs = with nixpkgs; {
     hs-abci-sdk = [protobuf];
     hs-abci-types = [protobuf];
     hs-iavl-client = [protobuf];
@@ -76,26 +72,10 @@ let
 
   addBuildInputs = inputs: { buildInputs ? [], ... }: { buildInputs = inputs ++ buildInputs; };
 
-  hackageOverrides = self: super: {};
-
-  localOverrides = self: super:
-    builtins.mapAttrs (name: path: (self.callCabal2nix name (gitignoreSource path) {})) packages;
-
-  repoOverrides = self: super:
-    builtins.mapAttrs (name: path: (self.callCabal2nix name path {})) repoPackages;
-
-  overrides = self: super:
-    let allOverrides =
-          hackageOverrides self super
-          // repoOverrides self super
-          // localOverrides self super;
-    in
-      builtins.mapAttrs (name: pkg: pkg.overrideAttrs (addBuildInputs (extra-build-inputs.${name} or []))) allOverrides;
-
   # TODO: figure out why running tests on mac causes builds to hang _after_ running the tests
-  keplerTests = pkg: args: if pkgs.stdenv.isDarwin then pkgs.haskell.lib.dontCheck pkg else keplerTests' pkg args;
+  keplerTests = pkg: args: if nixpkgs.stdenv.isDarwin then nixpkgs.haskell.lib.dontCheck pkg else keplerTests' pkg args;
 
-  keplerTests' = pkg: { runIavl ? false, runABCI ? null, runTendermint ? null }: pkgs.lib.overrideDerivation pkg (drv:
+  keplerTests' = pkg: { runIavl ? false, runABCI ? null, runTendermint ? null }: nixpkgs.lib.overrideDerivation pkg (drv:
     let
       iavlScript = ''
         ${iavl}/bin/iavlserver -db-name "test" -datadir "." -grpc-endpoint "0.0.0.0:8090" -gateway-endpoint "0.0.0.0:8091" &
@@ -111,52 +91,56 @@ let
         sleep 3
       '';
     in {
-      checkPhase = pkgs.lib.concatStrings [
-        (pkgs.lib.optionalString runIavl iavlScript)
-        (pkgs.lib.optionalString (runABCI != null) abciScript)
-        (pkgs.lib.optionalString (runTendermint != null) tendermintScript)
+      checkPhase = with nixpkgs.lib; concatStrings [
+        (optionalString runIavl iavlScript)
+        (optionalString (runABCI != null) abciScript)
+        (optionalString (runTendermint != null) tendermintScript)
         drv.checkPhase
       ];
     });
+
+  haskellOverrides = nixpkgs.lib.foldr nixpkgs.lib.composeExtensions (_: _: {}) [
+    (self: super: builtins.mapAttrs
+      (name: path: (self.callCabal2nix name (gitignoreSource path) {}).overrideAttrs (addBuildInputs (extra-build-inputs.${name} or [])))
+      packages
+    )
+    (self: super: with nixpkgs.haskell.lib; {
+      avl-auth = dontCheck (self.callCabal2nix "avl-auth" repos.avl-auth {});  # https://github.com/haskell-haskey/xxhash-ffi/issues/2
+
+      http2-client = doJailbreak (self.callCabal2nix "http2-client" repos.http2-client {});
+      http2-client-grpc = doJailbreak (self.callCabal2nixWithOptions "http2-client-grpc" repos.http2-grpc-haskell "--subpath http2-client-grpc" {});
+
+      http2-grpc-proto-lens = doJailbreak (unmarkBroken super.http2-grpc-proto-lens);
+      http2-grpc-types = doJailbreak (unmarkBroken super.http2-grpc-types);
+      large-generics = doJailbreak (unmarkBroken super.large-generics);
+      large-records = doJailbreak (self.callCabal2nixWithOptions "large-records" repos.large-records "--subpath large-records" {});
+
+      proto-lens-arbitrary = unmarkBroken super.proto-lens-arbitrary;
+      proto3-suite = dontCheck (doJailbreak super.proto3-suite); # Test suite fails to build with module discovery errors
+      proto3-wire = doJailbreak (unmarkBroken super.proto3-wire);
+    })
+  ];
 
   overlay = self: super: {
     inherit iavl tendermint;
 
     haskellPackages =
-      super.haskellPackages.override (old: {
-        overrides = pkgs.lib.foldr pkgs.lib.composeExtensions (old.overrides or (_: _: {})) [
-          overrides
-          (self: super: with pkgs.haskell.lib; {
-            avl-auth = dontCheck super.avl-auth;  # https://github.com/haskell-haskey/xxhash-ffi/issues/2
-
-            http2-client = doJailbreak (self.callCabal2nix "http2-client" repos.http2-client {});
-            http2-client-grpc = doJailbreak (self.callCabal2nixWithOptions "http2-client-grpc" repos.http2-grpc-haskell "--subpath http2-client-grpc" {});
-
-            http2-grpc-proto-lens = doJailbreak (unmarkBroken super.http2-grpc-proto-lens);
-            http2-grpc-types = doJailbreak (unmarkBroken super.http2-grpc-types);
-            large-generics = doJailbreak (unmarkBroken super.large-generics);
-            large-records = doJailbreak (self.callCabal2nixWithOptions "large-records" repos.large-records "--subpath large-records" {});
-
-            proto-lens-arbitrary = unmarkBroken super.proto-lens-arbitrary;
-            proto3-suite = dontCheck (doJailbreak super.proto3-suite); # Test suite fails to build with module discovery errors
-            proto3-wire = doJailbreak (unmarkBroken super.proto3-wire);
-          }
-        )
-      ];
+      super.haskellPackages.override (_old: {
+        overrides = haskellOverrides;
     });
   };
 
   overlays = [overlay];
 
 in rec {
-  inherit pkgs overlays;
+  inherit nixpkgs overlays;
 
   buildInputs = {
-    inherit (pkgs) /*iavl*/ protobuf tendermint;
-    inherit (pkgs.haskellPackages) cabal-install ghcid hlint stack stylish-haskell weeder;
+    inherit (nixpkgs) /*iavl*/ protobuf tendermint;
+    inherit (nixpkgs.haskellPackages) cabal-install ghcid hlint stack stylish-haskell weeder;
   };
 
-  keplerPackages = keplerPackages' pkgs.haskellPackages;
+  keplerPackages = keplerPackages' nixpkgs.haskellPackages;
   keplerPackages' = p: with p; {
     inherit
       hs-abci-extra
--- nix/shell.nix
+++ nix/shell.nix
@@ -1,6 +1,6 @@
 {}:
 let project = (import ./project.nix {});
-in project.pkgs.haskellPackages.shellFor {
+in project.nixpkgs.haskellPackages.shellFor {
   withHoogle = true;
   packages = p: builtins.attrValues (project.keplerPackages' p);
   nativeBuildInputs = builtins.attrValues project.buildInputs;
